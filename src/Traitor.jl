5e64776804f716a0a2b9624f79a3d9fbed8874ef
Chris Foster <chris.foster@roames.com.au>
2016-06-26 23:23:54 -0400

    Trait expression parser for function arguments

--- src/Traitor.jl
+++ src/Traitor.jl
@@ -1,5 +1,41 @@
 module Traitor
 
-# package code goes here
+# Extract `(normal_argument, trait)` from a function argument expression.
+# `normal_argument` is the standard argument expr without the trait.  If no
+# trait is present in the expression, return `nothing` for `trait`.
+#
+# :(x::Int::Big) -> (:(x::Int), :Big)
+function extract_arg_trait(ex)
+    if isa(ex, Symbol)
+        return ex, nothing # argument with no type annotation
+    end
+    if ex.head == :kw # default values, eg f(x::T::A=1)
+        arg, traittype = extract_arg_trait(ex.args[1])
+        return Expr(:kw, arg, ex.args[2]), traittype
+    end
+    @assert ex.head == :(::)
+    isfourcolon = isa(ex.args[2],Expr) && ex.args[2].head == :(::) # x::::A syntax
+    if isa(ex.args[1], Symbol) && !isfourcolon
+        return ex, nothing # argument with normal type annotation
+    end
+    if isfourcolon
+        return ex.args[1], ex.args[2].args[1]
+    end
+    normalarg = ex.args[1]
+    @assert normalarg.head == :(::)
+    traittype = ex.args[2]
+    return normalarg, traittype
+end
+
+#=
+macro traitor(ex)
+    if ex.head != :function
+        error("trait expression must be a function")
+    end
+    def = ex.args[1]
+    body = ex.args[2]
+    @assert def.head == :call
+end
+=#
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using Traitor
-using Base.Test
+using Traitor: extract_arg_trait
+using BaseTestNext
 
-# write your own tests here
-@test 1 == 1
+# Parsing of function argument trait constraints
+# Simple trait & type symbols
+@test extract_arg_trait(:(x::T::A)) == (:(x::T), :A)
+@test extract_arg_trait(:(x::::A))  == (:(x),    :A)
+@test extract_arg_trait(:(x::T))    == (:(x::T), nothing)
+@test extract_arg_trait(:(x))       == (:x,      nothing)
+# Type exprs
+@test extract_arg_trait(:(x::Union{S,T}::A)) == (:(x::Union{S,T}), :A)
+@test extract_arg_trait(:(x::Union{S,T}))    == (:(x::Union{S,T}), nothing)
+# Trait exprs
+@test extract_arg_trait(:(x::T::Union{A,B})) == (:(x::T), :(Union{A,B}))
+@test extract_arg_trait(:(x::::Union{A,B}))  == (:(x),    :(Union{A,B}))
+# Trait & type exprs
+@test extract_arg_trait(:(x::Union{S,T}::Union{A,B})) == (:(x::Union{S,T}), :(Union{A,B}))
+@test extract_arg_trait(:(x::::Union{A,B}))  == (:(x),    :(Union{A,B}))
+@test extract_arg_trait(:(x::Union{S,T}))    == (:(x::Union{S,T}), nothing)
+# Keyword args
+@test extract_arg_trait(Expr(:kw,:(x::T::A),1)) == (Expr(:kw,:(x::T),1), :A)
+@test extract_arg_trait(Expr(:kw,:(x::::A),1))  == (Expr(:kw,:x,1),      :A)
+@test extract_arg_trait(Expr(:kw,:(x::T),1))    == (Expr(:kw,:(x::T),1), nothing)
+@test extract_arg_trait(Expr(:kw,:x,1))         == (Expr(:kw,:x,1),      nothing)
+
