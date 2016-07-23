0d1d67886691ddbf7dd2fc94a364ab67cb241588
Tim Holy <tim.holy@gmail.com>
2016-07-23 10:06:34 -0500

    Steal working tests from the README

---
+++ test/runtests.jl
@@ -0,0 +1,37 @@
+using Traitor
+using Base.Test
+
+abstract Size
+
+immutable Big <: Size; end
+immutable Medium <: Size; end
+immutable Small <: Size; end
+
+Size(::Union{Type{Int32},Type{Int64}}) = Small
+Size(::Type{BigInt}) = Big
+Size(::Type{Int128}) = Medium
+
+@traitor function howbig(x::Any::Big)
+    "Huge!"
+end
+@traitor function howbig(x::::Medium)
+    "So-so"
+end
+@test howbig(Int128(1)) == "So-so"
+@test howbig(BigInt(1)) == "Huge!"
+
+@traitor function howbig(x::Integer::Small)
+    "Teensy..."
+end
+
+@test howbig(1) == "Teensy..."
+# These next ones throw...
+# @test howbig(Int128(1)) == "So-so"
+# @test howbig(BigInt(1)) == "Huge!"
+# ...until we define
+@traitor howbig(x::Integer::Big) = "Huge!"
+@traitor howbig(x::Integer::Medium) = "So-so"
+
+@test howbig(1) == "Teensy..."
+@test howbig(Int128(1)) == "So-so"
+@test howbig(BigInt(1)) == "Huge!"
